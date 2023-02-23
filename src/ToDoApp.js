225ac427c78c2203ec3fc17f1fce4a66cb4738c9
NONSTOP\Test <Behappy13@bk.ru>
2023-02-24 00:15:23 +0300

    Test_page

---
+++ src/ToDoApp.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+
+
+
+function TodoApp() {
+    return (
+           
+      <h1>Test Page.</h1>
+  
+
+    )
+    
+}
+
+
+ 
+  export default TodoApp;
